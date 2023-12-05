5889713605e8c171001c9a2acfc2d1e1ebd39924
nish404 <77575013+nish404@users.noreply.github.com>
2023-12-05 10:20:12 -0500

    Create GETLATESTBLOB.ps1

---
+++ GETLATESTBLOB.ps1
@@ -0,0 +1,49 @@
+# GLOBAL VARIABLES 
+$resourceGroupName = "YourResourceGroup"
+$storageAccountName = "YourStorageAccount"
+$containerName = "YourContainer"
+$folderPath = "YourFolderPath" 
+$context = ""
+$keyVault = ""
+$secret = ""
+
+<#
+  IF A FOLDER HAS A PATH YOU CAN USE THE FOLLOWING 
+  $TOPLEVELFOLDER = "< PARENT DIRECTORY >"
+
+  IF CONTAINER HAS MULTIPLE FOLDERS CREATE AN ARRAY
+  $FOLDERNAMES = @("< LIST FOLDERS HERE >", "< ANOTHER FOLDER >")
+
+  FOREACH ($FOLDER IN $FOLDERS) 
+  {
+    $BLOB = GET-AZSTORAGEBLOB -CONTAINER $CONTAINERNAME -CONTEXT $CONTEXT -PREFIX $TOPLEVELFOLDER/$FOLDER | SELECT -LAST 1
+  }
+#>
+
+# SIGN INTO YOUR AZURE ACCOUNT 
+Connect-AzAccount
+
+# SET THE CONTEXT FOR YOUR STORAGE ACCOUNT 
+$getSasToken = Get-AzKeyVault -VaultName $keyVault -Name $secret -AsPlainText
+$context = New-AzStorageContext -StorageAccountName $storageAccountName -SasToken $getSasToken
+
+# LIST BLOBS WITHIN THE SPECIFIED FOLDER 
+$blobs = Get-AzStorageBlob -Container $containerName -Context $context -Prefix $topLevelFolder/$folder
+
+# SORT THE BLOBS BY LAST MODIFIED TIMESTAMP IN DESCENDING ORDER 
+$latestBlob = $blobs | Sort-Object LastModified -Descending | Select-Object -First 1
+
+# DISPLAY INFORMATION ABOUT THE LATEST BLOB 
+if($latestBlob) {
+    Write-Host "LATEST VERSION OF '$topLevelFolder' IN '$container' CONTAINER OF '$storageAccount':" -ForegroundColor Yellow
+    Write-Host "    NAME: $($latestBlob.Name)"
+    Write-Host "    BLOB: $($latestBlob.BlobType)"
+    Write-Host "    SIZE: $($latestBlob.Length) BYTES"
+    Write-Host "    LAST MODIFIED: $($latestBlob.LastModified)"
+    Write-Host ""
+} else {
+    Write-Host "NO BLOB FOUND IN '$containerName' CONTAINER OF '$storageAccountName'" -ForegroundColor Red
+}
+
+# OUTPUT THE DETAILS OF THE MOST RECENT BLOB 
+# return $latestBlob
